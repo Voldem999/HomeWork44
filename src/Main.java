6ec162a2e163fcbaeaad3b4186dfd68be38bdc05
Voldem999 <vs-kat@mail.ru>
2022-08-26 10:48:27 +0700

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        //Задача 1
+//        int i = 1;
+//        while (i<=10) {System.out.println(i); i++;}
+
+//        int a = 10;
+//        for (a=10; a>=1; a--) {System.out.println(a);}
+
+        //Задача 2
+
     }
 }
